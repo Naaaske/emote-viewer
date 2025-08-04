7ad1bd66e9ea1da1b5a6acc821525a8e6f69d79f
Naske <thomasstubkjaer@gmail.com>
2025-08-04 22:30:12 +0200

    fix some stuff and add onlyListed param

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const url = new URL(window.location.href);
 
+const paramChannel = url.searchParams.get('channel') || url.searchParams.get('c')
+const paramOnlyListed = url.searchParams.has('onlyListed')
+
 const config = {
-  channel: url.searchParams.get("c"),
+  channel: paramChannel || 'northernlion',
   currentEmote: { emote: "", url: "" },
   emotes: [],
 };
 
-const getEmotes = async () => {
+const init = async () => {
   // const proxy = "https://tpbcors.herokuapp.com/";
   const proxy = "https://api.roaringiron.com/proxy/";
-  const paramChannel = url.searchParams.get("c")
-
-  config.channel = paramChannel ?? "naaaske"
 
   const twitchId = (
     await (
@@ -25,131 +25,130 @@ const getEmotes = async () => {
   )?.[0].id;
 
   await fetch(proxy + "https://api.frankerfacez.com/v1/room/" + config.channel)
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    const emoteNames = Object.keys(data.sets);
-    for (let i = 0; i < emoteNames.length; i++) {
-      for (let j = 0; j < data.sets[emoteNames[i]].emoticons.length; j++) {
-        const emote = data.sets[emoteNames[i]].emoticons[j];
-        config.emotes.push({
-          name: emote.name,
-          url:
-            "https://" +
-            (emote.urls["4"] || emote.urls["2"] || emote.urls["1"]).split("//").pop(),
-        });
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      const emoteNames = Object.keys(data.sets);
+      for (let i = 0; i < emoteNames.length; i++) {
+        for (let j = 0; j < data.sets[emoteNames[i]].emoticons.length; j++) {
+          const emote = data.sets[emoteNames[i]].emoticons[j];
+          config.emotes.push({
+            name: emote.name,
+            url:
+              "https://" +
+              (emote.urls["4"] || emote.urls["2"] || emote.urls["1"]).split("//").pop(),
+          });
+        }
       }
-    }
-  })
-  .catch();
+    })
+    .catch();
 
   await fetch(proxy + "https://api.frankerfacez.com/v1/set/global")
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    const emoteNames = Object.keys(data.sets);
-    for (let i = 0; i < emoteNames.length; i++) {
-      for (let j = 0; j < data.sets[emoteNames[i]].emoticons.length; j++) {
-        const emote = data.sets[emoteNames[i]].emoticons[j];
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      const emoteNames = Object.keys(data.sets);
+      for (let i = 0; i < emoteNames.length; i++) {
+        for (let j = 0; j < data.sets[emoteNames[i]].emoticons.length; j++) {
+          const emote = data.sets[emoteNames[i]].emoticons[j];
+          config.emotes.push({
+            name: emote.name,
+            url:
+              "https://" +
+              (emote.urls["4"] || emote.urls["2"] || emote.urls["1"]).split("//").pop(),
+          });
+        }
+      }
+    })
+    .catch();
+
+  await fetch(proxy + "https://api.betterttv.net/3/cached/users/twitch/" + twitchId)
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      for (let i = 0; i < data.channelEmotes.length; i++) {
         config.emotes.push({
-          name: emote.name,
-          url:
-            "https://" +
-            (emote.urls["4"] || emote.urls["2"] || emote.urls["1"]).split("//").pop(),
+          name: data.channelEmotes[i].code,
+          url: `https://cdn.betterttv.net/emote/${data.channelEmotes[i].id}/3x`,
         });
       }
-    }
-  })
-  .catch();
-  
-  await fetch(proxy + "https://api.betterttv.net/3/cached/users/twitch/" + twitchId)
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    for (let i = 0; i < data.channelEmotes.length; i++) {
-      config.emotes.push({
-        name: data.channelEmotes[i].code,
-        url: `https://cdn.betterttv.net/emote/${data.channelEmotes[i].id}/3x`,
-      });
-    }
-    for (let i = 0; i < data.sharedEmotes.length; i++) {
-      config.emotes.push({
-        name: data.sharedEmotes[i].code,
-        url: `https://cdn.betterttv.net/emote/${data.sharedEmotes[i].id}/3x`,
-      });
-    }
-  }) 
-  .catch();
+      for (let i = 0; i < data.sharedEmotes.length; i++) {
+        config.emotes.push({
+          name: data.sharedEmotes[i].code,
+          url: `https://cdn.betterttv.net/emote/${data.sharedEmotes[i].id}/3x`,
+        });
+      }
+    })
+    .catch();
 
   await fetch(proxy + "https://api.betterttv.net/3/cached/emotes/global")
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    for (let i = 0; i < data.length; i++) {
-      config.emotes.push({
-        name: data[i].code,
-        url: `https://cdn.betterttv.net/emote/${data[i].id}/3x`,
-      });
-    }
-  })
-  .catch();
-
-  await fetch(proxy + "https://7tv.io/v3/users/twitch/" + twitchId)
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    const emoteSet = data["emote_set"];
-    if (emoteSet === null) return;
-    const emotes = emoteSet["emotes"];
-    for (let i = 0; i < emotes.length; i++) {
-      const url = emotes[i].data.host.url;
-      const fileExt = emotes[i].data.host.files[3]?.name;
-      if (url && fileExt){
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      for (let i = 0; i < data.length; i++) {
         config.emotes.push({
-          name: emotes[i].name,
-          url:
-          "https:" +
-          url +
-          "/" +
-          fileExt,
+          name: data[i].code,
+          url: `https://cdn.betterttv.net/emote/${data[i].id}/3x`,
         });
       }
-    }
-  })
-  .catch();
+    })
+    .catch();
+
+  await fetch(proxy + "https://7tv.io/v3/users/twitch/" + twitchId)
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      const emoteSet = data["emote_set"];
+      if (emoteSet === null) return;
+      const emotes = emoteSet["emotes"];
+      for (let i = 0; i < emotes.length; i++) {
+        if (paramOnlyListed && emotes[i].data.listed === false) {
+          continue;
+        }
+        const url = emotes[i].data.host.url;
+        const fileExt = emotes[i].data.host.files[3]?.name;
+        if (url && fileExt) {
+          config.emotes.push({
+            name: emotes[i].name,
+            url: `https:${url}/4x.webp`
+          });
+        }
+      }
+    })
+    .catch();
 
   await fetch(proxy + "https://7tv.io/v3/emote-sets/global")
-  .then(response => {
-    if (!response.ok) return;
-    return response.json()
-  })
-  .then(data => {
-    if (!data) return;
-    for (let i = 0; i < data.emotes.length; i++) {
-      config.emotes.push({
-        name: data.emotes[i].name,
-        url: `https://cdn.7tv.app/emote/${data.emotes[i].id}/4x.webp`,
-      });
-    }
-  })
-  .catch();
+    .then(response => {
+      if (!response.ok) return;
+      return response.json()
+    })
+    .then(data => {
+      if (!data) return;
+      for (let i = 0; i < data.emotes.length; i++) {
+        config.emotes.push({
+          name: data.emotes[i].name,
+          url: `https://cdn.7tv.app/emote/${data.emotes[i].id}/4x.webp`,
+        });
+      }
+    })
+    .catch();
 
-  const message = `Successfully loaded ${config.emotes.length} emotes for channel ${config.channel}` + (!paramChannel ? `<br>If you want another channel, enter it in the URL. Example: https://naske.chat?c=naaaske` : ``);
+  const message = `Successfully loaded ${config.emotes.length} emotes for channel ${config.channel}` + (!paramChannel ? `<br>If you want another channel, enter it in the URL. Example: https://naske.chat?c=${config.channel}` : ``);
 
   $("#errors").append().html(message).delay(paramChannel ? 2000 : 10000).fadeOut(300);
   console.log(message, config.emotes);
@@ -170,8 +169,8 @@ const findEmotes = (message, rawMessage) => {
   const emoteUsedPos = rawMessage[4].startsWith("emotes=")
     ? 4
     : rawMessage[5].startsWith("emote-only=")
-    ? 6
-    : 5;
+      ? 6
+      : 5;
 
   const emoteUsed = rawMessage[emoteUsedPos].split("emotes=").pop();
   const splitMessage = message.split(" ").filter((a) => !!a.length);
@@ -184,12 +183,11 @@ const findEmotes = (message, rawMessage) => {
       parseInt(emoteUsed.split(":")[1].split("-")[0]),
       parseInt(emoteUsed.split(":")[1].split("-")[1]) + 1
     );
-    config.currentEmote.url = `https://static-cdn.jtvnw.net/emoticons/v2/${
-      emoteUsed.split(":")[0]
-    }/default/dark/4.0`;
+    config.currentEmote.url = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteUsed.split(":")[0]
+      }/default/dark/4.0`;
   } else {
     const emoteObj = findEmoteObjectInMessage(splitMessage);
-    if(emoteObj !== null){
+    if (emoteObj !== null) {
       config.currentEmote.emote = emoteObj.name;
       config.currentEmote.url = emoteObj.url;
     }
@@ -214,6 +212,7 @@ const connect = () => {
   }, 10000);
 
   chat.onopen = function () {
+    init();
     clearInterval(timeout);
     chat.send(
       "CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership"
@@ -222,7 +221,6 @@ const connect = () => {
     chat.send("NICK justinfan123");
     chat.send("JOIN #" + config.channel);
     console.log("Connected to Twitch IRC");
-    getEmotes();
   };
 
   chat.onerror = function () {
